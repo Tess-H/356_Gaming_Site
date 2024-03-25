cead4e5afac881f7db4b6de956f2615e609077ff
Alexis Hovorka <alexisspacegirl@gmail.com>
2024-03-25 10:31:52 -0600

    Add initial unclickable header

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,13 @@
 
     return (
       <>
-        <div>
-
-        </div>
+        <header>
+          <span id="menu-button" className="material-symbols-outlined">menu</span>
+          <h1>GameSearch</h1>
+          <div id="search-bar">
+            <span id="search-button" className="material-symbols-outlined">search</span>
+          </div>
+        </header>
         <GameCardsContainer games={games} />
 
       </>
