6622315344e59a103f6deada355672a042fa3a3b
Alexis Hovorka <alexisspacegirl@gmail.com>
2024-03-25 11:24:52 -0600

    Add expandable search input box
    Note: Styles for the box aren't currently suitable for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,34 @@
-  import "./App.css";
-  import GameCardsContainer from "./components/model/card/GameCardsContainer";
-  import { games } from "./components/model/service/gamesService";
+import React, { useState, useRef } from 'react';
+import "./App.css";
+import GameCardsContainer from "./components/model/card/GameCardsContainer";
+import { games } from "./components/model/service/gamesService";
 
-  function App() {
+function App() {
+  const searchInput = useRef(null);
+  const [isSearchOpen, setSearchOpen] = useState(false);
+  const toggleSearch = () => {
+    setSearchOpen(!isSearchOpen);
+    searchInput.current[!isSearchOpen? "focus" : "blur"]();
+  };
 
-    return (
-      <>
-        <header>
-          <span id="menu-button" className="material-symbols-outlined">menu</span>
-          <h1>GameSearch</h1>
-          <div id="search-bar">
-            <span id="search-button" className="material-symbols-outlined">search</span>
+  return (
+    <>
+      <header>
+        <span id="menu-button" className="material-symbols-outlined header-button">menu</span>
+        <h1>Game Search</h1>
+        <div id="search-bar">
+          <span id="search-button" className="material-symbols-outlined header-button" onClick={toggleSearch}>search</span>
+          <div id="search-collapse" className={isSearchOpen? "show" : ""}>
+            <input id="search-input" ref={searchInput} type="search" placeholder="Search" />
+            <span id="search-go" className="material-symbols-outlined header-button">send</span>
           </div>
-        </header>
-        <GameCardsContainer games={games} />
+          <span id="random-button" className="material-symbols-outlined header-button">casino</span>
+        </div>
+      </header>
+      <GameCardsContainer games={games} />
 
-      </>
-    );
-  }
+    </>
+  );
+}
 
-  export default App;
+export default App;
